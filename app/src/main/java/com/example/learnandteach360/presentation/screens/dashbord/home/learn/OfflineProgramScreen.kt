f08f3a4bb8d5bb1170948b1affcaf3009e55e63a
Anurag Sen <anurag007sen@gmail.com>
2025-02-22 20:07:19 +0530

    working on program card

--- app/src/main/java/com/example/learnandteach360/presentation/screens/dashbord/home/learn/OfflineProgramScreen.kt
+++ app/src/main/java/com/example/learnandteach360/presentation/screens/dashbord/home/learn/OfflineProgramScreen.kt
@@ -1,15 +1,34 @@
 package com.example.learnandteach360.presentation.screens.dashbord.home.learn
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.learnandteach360.presentation.screens.dashbord.home.learn.componests.ProgramCard
 
 @Composable
 fun OfflineProgramScreen(modifier: Modifier = Modifier) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-        Text(text = "Offline Program screen")
+    Column(modifier = Modifier.fillMaxSize()){
+        ProgramCard()
+        Spacer(modifier = Modifier.height(10.dp))
+        ProgramCard()
+        Spacer(modifier = Modifier.height(10.dp))
+        ProgramCard()
+        Spacer(modifier = Modifier.height(10.dp))
+        ProgramCard()
+        Spacer(modifier = Modifier.height(10.dp))
     }
+}
+
+@Preview
+@Composable
+private fun OfflineProgramPreview() {
+    OfflineProgramScreen()
 }
